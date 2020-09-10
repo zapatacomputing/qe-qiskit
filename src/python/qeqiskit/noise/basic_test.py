a6660fbab0d2e337b6818cce74b80a0bd15317ec
Amara Katabarwa <Henriamaa@gmail.com>
2020-09-10 13:43:10 -0400

    adding new import to fix bug

--- src/python/qeqiskit/noise/basic_test.py
+++ src/python/qeqiskit/noise/basic_test.py
@@ -8,7 +8,7 @@ from .basic import (get_qiskit_noise_model,
                     create_amplitude_damping_noise, 
                     create_phase_damping_noise, create_phase_and_amplitude_damping_error, 
                     create_pta_channel, get_kraus_matrices_from_noise_model)
-
+from typing import Dict
 
 class TestBasic(unittest.TestCase):
     def setUp(self):
