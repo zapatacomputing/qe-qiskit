77c9e367a7f5ba6554489cee9991039b8cc69151
Amara Katabarwa <Henriamaa@gmail.com>
2020-10-07 17:17:28 -0400

    testing saving kraus from workflow

--- src/python/qeqiskit/noise/basic_test.py
+++ src/python/qeqiskit/noise/basic_test.py
@@ -7,8 +7,7 @@ from zquantum.core.circuit import CircuitConnectivity
 from .basic import (get_qiskit_noise_model, 
                     create_amplitude_damping_noise, 
                     create_phase_damping_noise, create_phase_and_amplitude_damping_error, 
-                    create_pta_channel, get_kraus_matrices_from_noise_model, 
-                    get_kraus_matrices_from_ibm_noise_model)
+                    create_pta_channel, get_kraus_matrices_from_ibm_noise_model)
 
 class TestBasic(unittest.TestCase):
     def setUp(self):
