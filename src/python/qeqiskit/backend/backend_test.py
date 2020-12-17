ef205f0d252110446e4b1e0496cd06fe4bfc1b43
michalstechly <michal.stechly@gmail.com>
2020-12-17 17:49:20 -0500

    Updated code to be up to date with dev.

--- src/python/qeqiskit/backend/backend_test.py
+++ src/python/qeqiskit/backend/backend_test.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 import os
 from pyquil import Program
 from pyquil.gates import X, CNOT
@@ -10,36 +10,37 @@ from zquantum.core.interfaces.backend_test import QuantumBackendTests
 from .backend import QiskitBackend
 
 
-class TestQiskitBackend(unittest.TestCase, QuantumBackendTests):
-    def setUp(self):
-        ibmq_api_token = os.getenv("ZAPATA_IBMQ_API_TOKEN")
-        self.backends = [
-            QiskitBackend(
-                device_name="ibmq_qasm_simulator", n_samples=1, api_token=ibmq_api_token
-            ),
-        ]
+@pytest.fixture(
+    params=[
+        {
+            "device_name": "ibmq_qasm_simulator",
+            "n_samples": 1,
+            "api_token": os.getenv("ZAPATA_IBMQ_API_TOKEN"),
+        },
+    ]
+)
+def backend(request):
+    return QiskitBackend(**request.param)
 
-    def test_run_circuitset_and_measure(self):
-        for backend in self.backends:
 
-            # Given
-            num_circuits = 10
-            circuit = Circuit(Program(X(0), CNOT(1, 2)))
-            n_samples = 100
-            # When
-            backend.n_samples = n_samples
-            measurements_set = backend.run_circuitset_and_measure(
-                [circuit] * num_circuits
-            )
-            # Then
-            self.assertEqual(len(measurements_set), num_circuits)
-            for measurements in measurements_set:
-                self.assertEqual(len(measurements.bitstrings), n_samples)
+class TestQiskitBackend(QuantumBackendTests):
+    def test_run_circuitset_and_measure(self, backend):
+        # Given
+        num_circuits = 10
+        circuit = Circuit(Program(X(0), CNOT(1, 2)))
+        n_samples = 100
+        # When
+        backend.n_samples = n_samples
+        measurements_set = backend.run_circuitset_and_measure([circuit] * num_circuits)
+        # Then
+        assert len(measurements_set) == num_circuits
+        for measurements in measurements_set:
+            assert len(measurements.bitstrings) == n_samples
 
-                # Then (since SPAM error could result in unexpected bitstrings, we make sure the most common bitstring is
-                #   the one we expect)
-                counts = measurements.get_counts()
-                self.assertEqual(max(counts, key=counts.get), "100")
+            # Then (since SPAM error could result in unexpected bitstrings, we make sure the most common bitstring is
+            #   the one we expect)
+            counts = measurements.get_counts()
+            assert max(counts, key=counts.get) == "100"
 
     def test_readout_correction_works_run_circuit_and_measure(self):
         # Given
@@ -56,8 +57,8 @@ class TestQiskitBackend(unittest.TestCase, QuantumBackendTests):
         backend.run_circuit_and_measure(circuit)
 
         # Then
-        self.assertTrue(backend.readout_correction)
-        self.assertIsNotNone(backend.readout_correction_filter)
+        assert backend.readout_correction
+        assert backend.readout_correction_filter is not None
 
     def test_readout_correction_works_run_circuitset_and_measure(self):
         # Given
@@ -74,11 +75,10 @@ class TestQiskitBackend(unittest.TestCase, QuantumBackendTests):
         backend.run_circuitset_and_measure([circuit] * 10)
 
         # Then
-        self.assertTrue(backend.readout_correction)
-        self.assertIsNotNone(backend.readout_correction_filter)
+        assert backend.readout_correction
+        assert backend.readout_correction_filter is not None
 
     def test_device_that_does_not_exist(self):
         # Given/When/Then
-        self.assertRaises(
-            QiskitBackendNotFoundError, lambda: QiskitBackend("DEVICE DOES NOT EXIST")
-        )
+        with pytest.raises(QiskitBackendNotFoundError):
+            QiskitBackend("DEVICE DOES NOT EXIST")
