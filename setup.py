d4b8290731918232cadc748a4074b33cc50d29e0
Michał Stęchły <michal.stechly@gmail.com>
2021-03-03 12:58:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zapatacomputing/qe-qiskit",
-    packages=setuptools.find_namespace_packages(
-        include=["qeqiskit.*"], where="src/python"
-    ),
+    packages=setuptools.find_packages(where="src/python"),
     package_dir={"": "src/python"},
     classifiers=(
         "Programming Language :: Python :: 3",
