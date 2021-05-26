606d131d577893b4bd25cffaefb3ad2c2accf496
mstechly <michal.stechly@gmail.com>
2021-05-26 17:25:39 -0400

    Changed imports for circuit connectivity + added typing.

--- src/python/qeqiskit/noise/basic.py
+++ src/python/qeqiskit/noise/basic.py
@@ -2,7 +2,7 @@ import numpy as np
 import qiskit.providers.aer.noise as AerNoise
 from qiskit.providers.ibmq import IBMQ
 from qiskit.providers.ibmq.exceptions import IBMQAccountError
-from zquantum.core.circuit import CircuitConnectivity
+from zquantum.core.wip.circuits.layouts import CircuitConnectivity
 from qiskit.providers.aer.noise import (
     amplitude_damping_error,
     phase_damping_error,
@@ -11,22 +11,26 @@ from qiskit.providers.aer.noise import (
 )
 from qiskit.providers.aer.noise import NoiseModel
 from qiskit.quantum_info import Kraus
+from typing import Optional, Tuple, Dict
 
 
 def get_qiskit_noise_model(
-    device_name, hub="ibm-q", group="open", project="main", api_token=None
-):
+    device_name: str,
+    hub: str = "ibm-q",
+    group: str = "open",
+    project: str = "main",
+    api_token: Optional[str] = None,
+) -> Tuple(NoiseModel, CircuitConnectivity):
     """Get a qiskit noise model to use noisy simulations with a qiskit simulator
 
     Args:
-        device_name (string): The name of the device trying to be emulated
-        hub (string): The ibmq hub (see qiskit documentation)
-        group (string): The ibmq group (see qiskit documentation)
-        project (string): The ibmq project (see qiskit documentation)
-        api_token (string): The ibmq api token (see qiskit documentation)
-    Returns:
-        qiskit.providers.aer.noise.NoiseModel
-        zquantum.core.circuit.CircuitConnectivity: the qubit connectivity of the device
+        device_name: The name of the device trying to be emulated
+        hub: The ibmq hub (see qiskit documentation)
+        group: The ibmq group (see qiskit documentation)
+        project: The ibmq project (see qiskit documentation)
+        api_token: The ibmq api token (see qiskit documentation)
+
+
     """
     if api_token is not None and api_token is not "None":
         try:
@@ -48,15 +52,12 @@ def get_qiskit_noise_model(
     return noise_model, CircuitConnectivity(coupling_map)
 
 
-def create_amplitude_damping_noise(T_1, t_step=10e-9):
+def create_amplitude_damping_noise(T_1: float, t_step: float = 10e-9) -> NoiseModel:
     """Creates an amplitude damping noise model
 
     Args:
-        T_1 (float) : Relaxation time (seconds)
-        t_step (float) : Discretized time step over which the relaxation occurs over (seconds)
-
-    Returns:
-        qiskit.providers.aer.noise.NoiseModel
+        T_1: Relaxation time (seconds)
+        t_step: Discretized time step over which the relaxation occurs over (seconds)
     """
 
     gamma = 1 - pow(np.e, -1 / T_1 * t_step)
@@ -69,15 +70,12 @@ def create_amplitude_damping_noise(T_1, t_step=10e-9):
     return noise_model
 
 
-def create_phase_damping_noise(T_2, t_step=10e-9):
+def create_phase_damping_noise(T_2: float, t_step: float = 10e-9) -> NoiseModel:
     """Creates a dephasing noise model
 
     Args:
-        T_2 (float) : dephasing time (seconds)
-        t_step (float) : Discretized time step over which the relaxation occurs over (seconds)
-
-    Returns:
-        qiskit.providers.aer.noise.NoiseModel
+        T_2: dephasing time (seconds)
+        t_step: Discretized time step over which the relaxation occurs over (seconds)
     """
     gamma = 1 - pow(np.e, -1 / T_2 * t_step)
     error = phase_damping_error(gamma)
@@ -89,16 +87,15 @@ def create_phase_damping_noise(T_2, t_step=10e-9):
     return noise_model
 
 
-def create_phase_and_amplitude_damping_error(T_1, T_2, t_step=10e-9):
+def create_phase_and_amplitude_damping_error(
+    T_1: float, T_2: float, t_step: float = 10e-9
+) -> NoiseModel:
     """Creates a noise model that does both phase and amplitude damping
 
     Args:
-        T_1 (float) : Relaxation time (seconds)
-        T_2 (float) : dephasing time  (seonds)
-        t_step (float) : Discretized time step over which the relaxation occurs over (seconds)
-
-    Returns:
-        qiskit.providers.aer.noise.NoiseModel
+        T_1: Relaxation time (seconds)
+        T_2: dephasing time  (seonds)
+        t_step: Discretized time step over which the relaxation occurs over (seconds)
     """
 
     param_amp = 1 - pow(np.e, -1 / T_1 * t_step)
@@ -112,19 +109,16 @@ def create_phase_and_amplitude_damping_error(T_1, T_2, t_step=10e-9):
     return noise_model
 
 
-def create_pta_channel(T_1, T_2, t_step=10e-9):
+def create_pta_channel(T_1: float, T_2: float, t_step: float = 10e-9) -> NoiseModel:
     """Creates a noise model that does both phase and amplitude damping but in the
         Pauli Twirling Approximation discussed the following reference
         https://arxiv.org/pdf/1305.2021.pdf
 
 
     Args:
-        T_1 (float) : Relaxation time (seconds)
-        T_2 (float) : dephasing time (seconds)
-        t_step (float) : Discretized time step over which the relaxation occurs over (seconds)
-
-    Returns:
-        qiskit.providers.aer.noise.NoiseModel
+        T_1: Relaxation time (seconds)
+        T_2: dephasing time (seconds)
+        t_step: Discretized time step over which the relaxation occurs over (seconds)
     """
 
     p_x = 0.25 * (1 - pow(np.e, -t_step / T_1))
@@ -144,11 +138,11 @@ def create_pta_channel(T_1, T_2, t_step=10e-9):
     return noise_model
 
 
-def get_kraus_matrices_from_ibm_noise_model(noise_model):
+def get_kraus_matrices_from_ibm_noise_model(noise_model: NoiseModel) -> Dict:
     """Gets the kraus operators from a pre defined noise model
 
     Args:
-        noise_model (qiskit.providers.aer.noise.NoiseModel): Noise model for circuit
+        noise_model: Noise model for circuit
 
     Return
         dict_of_kraus_operators(dict): A dictionary labelled by keys which are the basis gates and values are the list of kraus operators
