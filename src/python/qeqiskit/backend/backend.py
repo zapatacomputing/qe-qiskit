f035d0570101adb503ac528e3175e3a2737ee583
Amara Katabarwa <Henriamaa@gmail.com>
2022-04-14 11:40:05 -0400

    debugging

--- src/python/qeqiskit/backend/backend.py
+++ src/python/qeqiskit/backend/backend.py
@@ -341,6 +341,8 @@ class QiskitBackend(QuantumBackend):
         """
 
         start_time = time.time()
+        for b in batch:
+            print(b.draw())
         while True:
             try:
                 job = execute(
